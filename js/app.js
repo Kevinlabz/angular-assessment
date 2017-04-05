dd08980ad846a2fb83b2d8e2e8d190f3f627827b
Kevin Labs <kevinlabs@outlook.com>
2017-04-05 14:25:54 -0600

    Some progress captured

---
+++ js/app.js
@@ -0,0 +1,25 @@
+angular.module('assessment', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        //If unkonwn url is reached instead of showing 404 page. It will bring back to the homepage.
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './../views/home.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: './../views/about.html'
+            })
+            .state('blog', {
+                url: '/blog',
+                templateUrl: './../views/blog.html'
+            })
+            .state('shop', {
+                url: '/shop',
+                templateUrl: './../views/shop.html'
+            })
+
+    });
